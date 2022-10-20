beedefc4fc6f15df9255203c9f2076dc4211a13e
Julian4047 <you@example.com>
2022-10-20 00:08:52 -0300

    bvtest

---
+++ archive/03-03-2022/main.js
@@ -0,0 +1,4 @@
+const iframe = document.getElementById("domainLooker");
+const iframeW = iframe.contentWindow;
+const iframeD = iframe.contentDocument;
+iframeD.querySelector(".btn-orange").style.backgroundColor = "red";
